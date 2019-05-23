c4ea07261b611e2b2f20870a9e967980da4abe38
Joel Stewart <joel@codeinversion.com>
2019-05-22 20:29:49 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# irc-laprf-gateway
-Gateway for ImmersionRC Timers. Allows multiple connections to the LapRF Personal Timer (Puck) and the 8-way Event Timer.
+# ImmersionRC LapRF Gateway
+
+This Node.js app sits in front of an 8-Way Event Timer or a LapRF Personal Timer ("Puck") and allows multiple connections to the device. It also converts the Puck, when connected to the host over USB, to an Ethernet connection for improved reliability and throughput.
+
+This application can be run on any device with Node.js installed, including:
+- A Raspberry Pi, including the Zero W (ideal for turning the Puck into a WiFi device)
+- Your current laptop
+
+# Raspberry Pi Setup
+
+I recommend the Raspbian Stretch Lite OS (smallest image, no desktop, terminal only).
+
+You will need to install Node.js manually (not using apt-get).
+- From the terminal, execute `uname -m`
+- Visit https://nodejs.org/en/download/
+- Download the appropriate LTS ARM version (ARM version from the output from uname)
+- `wget https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-armv6l.tar.xz`
+- Unzip the tar: `tar -xf node-v10.15.3-linux-armv7l.tar.xz`
+- Copy the binaries: `sudo cp -R node-v10.15.3-linux-armv7l/* /usr/local/`
+
+
+# Installation
+
+Clone this repository:
+
+Install dependencies: `npm install`
+
+Raspberry Pi Notes:
+- sudo apt-get install libusb-1.0-0-dev
+- sudo apt-get install libudev-dev
+
+If you are using the Event Timer, edit the index.js file and insert the static IP address of the timer.
+If you are using the Puck, set the Event Timer IP address to "".
+
+# Running
+
+Execute `node index.js` in the main folder.
+Connect to the Gateway instead of the devices directly.
---
+++ index.js
@@ -0,0 +1,127 @@
+// Replace this with your Event Timer IP Address (if present)
+const EventTimerIP = "192.168.1.9" // set to an empty string ("") to use the USB Puck
+
+// Imports
+const SerialPort = require('serialport')
+const Net = require('net');
+const USBDetect = require('usb-detection');
+
+// Networked Event Timer
+let eventTimer = null;
+// Local USB LapRF Personal Puck
+let puck = null;
+
+// Keep track of connections to the Puck / Event Timer
+const sockets = {};
+
+// Event Timer Connection (set EventTimerIP to "" if using the Puck. See above.)
+// The Event Timer must be connectable when you run the Gateway.
+// TODO: In the event of an error (not connectable), retry after 20 seconds...?
+if (EventTimerIP.length > 0) {
+    console.log('Attempting to connect to Event Timer: ' + EventTimerIP)
+    const timerOptions = {
+        host: EventTimerIP,
+        port: 5403
+    }
+    eventTimer = Net.createConnection(timerOptions, () => {
+        console.log('Event Timer Connected');
+        eventTimer.on('data', function (data) {    
+            console.log('Event Timer Data...')
+            // Forward all data (Buffer) to each client socket
+            for (var address in sockets) {
+                console.log('      ... Forwarding to ' + address);
+                let socket = sockets[address];
+                socket.write(data);
+            }
+        });
+        eventTimer.on('end', function () {
+            eventTimer = null
+        });
+    });
+
+} else {
+    // Not Connecting to the Event Timer, connect to USB Puck
+
+    // Open a Serial Port to the Puck at the specified location
+    function openPuck(location) {
+        console.log('Puck Connected');
+        puck = new SerialPort(location, {
+            baudRate: 115200    // The Puck operates at this speed. Do Not Change.
+        });
+        puck.on('data', function (data) {
+            console.log('Puck Data...');
+            // Forward all data (Buffer) to each client socket
+            for (var address in sockets) {
+                console.log('      ... Forwarding to ' + address);
+                let socket = sockets[address];
+                socket.write(data);
+            }
+        });
+    }
+
+    // Look for the Puck Serial Device
+    function findSerialDevice() {
+        SerialPort.list(function(err, ports) {
+            if (puck == null) {
+                ports.forEach(function(port) {
+                    // Only interested in the ImmersionRC Vendor and LapRF Puck Product
+                    if (port.vendorId === '04d8' & port.productId === '000a') {
+                        openPuck(port.comName)
+                    }
+                });
+            }
+        });
+    }
+
+    // Do an initial search for the USB Puck at launch
+    findSerialDevice()
+
+    // USB Detector for the Puck (Puck is not plugged in after launch of the Gateway)
+    USBDetect.startMonitoring();
+    // 1240:10 is the Vendor ID / Product ID of the ImmersionRC Puck
+    USBDetect.on('add:1240:10', function(device) {
+        // Need to delay a second as it takes a bit for the OS to identify the USB device's Serial Properties
+        setTimeout(findSerialDevice, 1000);
+    });
+    USBDetect.on('remove:1240:10', function(device) {
+        console.log("Puck Disconnected")
+        // TODO / BUG: If you plug a second Puck in and then remove it, it nulls out the first puck
+        // Work around: Don't do that.
+        puck = null
+    });
+}
+
+// Main server for client connections to the Gateway.
+const server = Net.createServer(client => {
+    // Client connected, store it where the Puck / Event Timer can get at it
+    sockets[client.remoteAddress] = client;
+    client.on('data', function (data) {    
+        console.log('Socket Data...')
+        if (eventTimer) {
+            console.log('      ... Forwarding to Event Timer');
+            eventTimer.write(data);
+        } else if (puck) {
+            console.log('      ... Forwarding to Puck');
+            puck.write(data);
+        }
+    });
+    client.on('error', (err) => {
+    // Errors Happen. Open an Issue on Github!
+        console.error(JSON.stringify(err));
+    });
+    client.on('end', function () {
+        // Remove this Client from the socket object
+        delete sockets[client.remoteAddress];
+    });
+});
+server.on('error', (err) => {
+    // Errors Happen. Open an Issue on Github!
+    console.error('TCP server: ' + JSON.stringify(err));
+});
+server.on('close', () => {
+    console.log('TCP server: Socket Closed.');
+});
+// Start listening on the ImmersionRC Network Port (5403)
+server.listen(5403, () => {
+    console.log('TCP server: ' + JSON.stringify(server.address()));
+});
